9c65b74b0ce333fa576971bb029ae95398d871e7
xt <359206421@qq.com>
2024-10-11 22:21:42 +0800

    feat：更新一下

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, ipcMain } from "electron";
+import { app, BrowserWindow, ipcMain, session } from "electron";
 import { join } from "path";
 import { Server } from "http";
 import startProxy from "./proxy";
@@ -15,6 +15,7 @@ const createWindow = () => {
     title: "Electron代理",
     paintWhenInitiallyHidden: false,
     webPreferences: {
+      allowRunningInsecureContent: true,
       preload: isPackaged
         ? join(__dirname, "../preload.js")
         : join(__dirname, "../preload/preload.js"),
@@ -24,9 +25,22 @@ const createWindow = () => {
   isPackaged
     ? win.loadFile("dist/render/index.html")
     : win.loadURL("http://localhost:8000");
+
+  win.webContents.openDevTools();
 };
 
+app.commandLine.appendSwitch('--ignore-certificate-errors', 'true')
+
 app.whenReady().then(() => {
+  // session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
+  //   callback({
+  //     responseHeaders: {
+  //       ...details.responseHeaders,
+  //       "Content-Security-Policy": "*", // ["default-src 'none'"],
+  //     },
+  //   });
+  // });
+
   createWindow();
 });
 
